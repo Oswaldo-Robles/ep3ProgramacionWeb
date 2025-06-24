0bb6614be5ed91f297acb5f5768a613c652709cc
Jesus Robles <jasasrobles02@gmail.com>
2025-06-24 13:00:43 -0500

    calcular total del carrito finalizado

--- js/pages/carrito.js
+++ js/pages/carrito.js
@@ -2,9 +2,15 @@
     let carrito = []
     const tbodyCarrito = document.getElementById("tbody-carrito")
     const btnVaciarCarrito = document.getElementById("btn-vaciar-carrito")
+    const totalPedido = document.getElementById("total-pedido")
 
     carrito = JSON.parse(sessionStorage.getItem("carritoCompra"))
-    
+
+    const calcularTotalCarrito = () => {
+        const subtotal = carrito.reduce((acumulador, item) => acumulador + item.precio * item.cantidad, 0);
+        totalPedido.textContent = subtotal.toFixed(2);
+    }
+
     const habilitarEliminarItem = () => {
         tbodyCarrito.querySelectorAll(".icono-eliminar").forEach((iconoEliminar, index) => {
             iconoEliminar.addEventListener("click", () => {
@@ -22,16 +28,18 @@
                 `<tr> 
                 <td>${item.idProducto}</td>
                 <td>${item.nombre}</td>
-                <td class= "text-end">${item.cantidad}</td>
-                <td class= "text-end">${item.precio * item.cantidad }</td>
                 <td class= "text-end">${item.precio.toFixed(2)}</td>
+                <td class= "text-end">${item.cantidad }</td>
+                <td class= "text-end">${item.precio.toFixed(2)*item.cantidad}</td>
                 <td> <i class="bi bi-x-circle icono-eliminar" title="Eliminar item"></i> </td>
             </tr>`
 
             tbodyCarrito.innerHTML += fila
         })
         habilitarEliminarItem();
+        calcularTotalCarrito();
     }
+
 
     dibujarCarrito();
 
@@ -39,6 +47,7 @@
         tbodyCarrito.innerHTML = "";
         sessionStorage.removeItem("carritoCompra")
         carrito = []
+        totalPedido.textContent = "0.00"
     })
 
 })()
