db80a6dcc14a99ed6a45ef5e661d22cf4b462646
Jesus Robles <jasasrobles02@gmail.com>
2025-06-29 23:20:26 -0500

    Tutotrial terminado

--- js/pages/iniciarSesion.js
+++ js/pages/iniciarSesion.js
@@ -1,6 +1,34 @@
 (() => {
     const formLogin = document.getElementById("form-login");
 
+    const mostrarPerfil = () =>{
+        fetch("pages/perfil.html")
+            .then((response) => response.text())
+            .then((data) => {
+                mainContent.innerHTML = data
+                const script = document.createElement("script")
+                script.src = "js/pages/perfil.js"
+                mainContent.appendChild(script)
+            })
+    }
+
+    const evaluarInicioSesion = (data) =>{
+        switch(data){
+            case -1:
+                alert("La cuenta no existe");
+                break;
+            case -2:
+                alert("La clave es incorrecta");
+                break;
+            default:
+                alert("Bienvenido");
+                mostrarPerfil();
+                sessionStorage.setItem("datosUser", JSON.stringify(data));
+
+                break;
+        }
+    }
+
     formLogin.addEventListener("submit", (event) => {
         event.preventDefault(); // ← importante!
         const formData = new FormData(formLogin);
@@ -14,6 +42,14 @@
             .then(data => {
                 console.log(data);
                 formLogin.reset();
+                evaluarInicioSesion(data);
             })
     });
+
+    document.getElementById("mostrar-Clave").addEventListener("change",(event)=>{
+        const inputClave = document.getElementById("input-clave");
+        inputClave.setAttribute("type", event.target.checked ? "text" : "password")
+    })
+
+
 })();
--- js/pages/perfil.js
+++ js/pages/perfil.js
@@ -1,3 +1,17 @@
 (()=>{
+    const datosUsuario = JSON.parse(sessionStorage.getItem("datosUser"))[0];
+
+    console.log(datosUsuario);
+
+/*     const tbodyUsuario = document.getElementById("tbody-usuario");
+    tbodyUsuario.querySelector("tr:nth-child(1) td").textContent = datosUsuario.nombres */
+
+
+    document.getElementById("td-nombre").textContent = datosUsuario.nombres;
+    document.getElementById("td-cargo").textContent = datosUsuario.cargo ;
+    document.getElementById("td-empresa").textContent = datosUsuario.cargo;
+    document.getElementById("td-correo").textContent = datosUsuario.correo;
+    document.getElementById("td-telefono").textContent = datosUsuario.telefono;
+    document.getElementById("td-pais").textContent = datosUsuario.pais;
 
 })()
